0cb3c888d797f1fe0d49c33e0550842f763d9ea4
michi-d <drews@neuro.mpg.de>
2020-07-11 23:32:12 +0200

    try alternative setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"michaelsdrews@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+" > ~/.streamlit/config.toml
